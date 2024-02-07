594a75636ccf3f29891a964ec68518614c139f68
Scooby doo <mikal.mckeever1@gmail.com>
2024-02-06 21:16:09 -0600

    1st Version of my App/Had to capitalize u in AboutUs route

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/about',
       name: 'about',
-      component: () => import('../views/Aboutus.vue')
+      component: () => import('../views/AboutUs.vue')
     },
     {
-      path: "/cart",
-      name: "cart",
-      component: () => import("../views/Cart.vue"),
+      path: '/cart',
+      name: 'cart',
+      component: () => import('../views/Cart.vue')
     },
     {
-      path: "/login",
-      name: "login",
-      component: () => import("../views/LogIn.vue"),
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LogIn.vue')
     },
     {
-      path: "/mensProducts",
-      name: "mensProducts",
-      component: () => import("../views/MensProducts.vue"),
+      path: '/mensProducts',
+      name: 'mensProducts',
+      component: () => import('../views/MensProducts.vue')
     },
     {
-      path: "/womensProducts",
-      name: "womensProducts",
-      component: () => import("../views/WomensProducts.vue"),
+      path: '/womensProducts',
+      name: 'womensProducts',
+      component: () => import('../views/WomensProducts.vue')
     }
   ]
 })
